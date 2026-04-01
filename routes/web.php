9326f2202dffb7cff070a88a3024f840945c97a9
Kipyegon Evans <kipyegonevan@gmail.com>
2026-04-01 14:15:21 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,19 @@ Route::post('/login',  [WebController::class, 'login'])->name('login.post');
 Route::get('/register',  [WebController::class, 'showRegister'])->name('register');
 Route::post('/register', [WebController::class, 'register'])->name('register.post');
 
+Route::get('/_health', fn () => response('ok', 200));
+Route::get('/_debug', function () {
+    return response()->json([
+        'APP_ENV' => env('APP_ENV', 'undefined'),
+        'APP_DEBUG' => env('APP_DEBUG', 'undefined'),
+        'APP_KEY' => env('APP_KEY', 'undefined'),
+        'DB_CONNECTION' => env('DB_CONNECTION', 'undefined'),
+        'DB_HOST' => env('DB_HOST', 'undefined'),
+        'DB_DATABASE' => env('DB_DATABASE', 'undefined'),
+        'php' => phpversion(),
+    ]);
+});
+
 /*
 |--------------------------------------------------------------------------
 | Protected routes (session JWT required)
